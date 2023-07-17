d67a1bedc03146ad4a15a22f14aec48ddc79ab83
L1ttel_Y <L1ttel_Y@126.com>
2023-07-18 00:28:18 +0800

    完成了摄像头控制器的功能

--- Assets/Gameplay/Placement/PlacementController.cs
+++ Assets/Gameplay/Placement/PlacementController.cs
@@ -8,32 +8,20 @@ namespace Gameplay.Placement {
 	public class PlacementController:MonoBehaviour {
 
 		[SerializeField] CommandData commandData;
+		CameraController cameraController;
 
-		new Camera camera;
 		private void Start() {
-			camera=GetComponent<Camera>();
+			cameraController=GetComponentInParent<CameraController>();
 		}
 
 		private void Update() {
-			Vector3 mouseOverPosition = Vector3.down*10;
-			Ray ray = camera.ScreenPointToRay(Input.mousePosition);
-			int cnt = Physics.RaycastNonAlloc(ray,UtilityPhysics.rayactBuffer);
-			for(int i = 0;i<cnt;i++) {
-				RaycastHit hit = UtilityPhysics.rayactBuffer[i];
-				if(hit.collider.GetComponent<GridObject>()) {
-					mouseOverPosition=hit.point;
-					break;
-				}
-			}
+			Vector3 mouseOverPosition = cameraController.mouseOverPosition;
 
 			if(mouseOverPosition.y>=0) {
 
 				Vector2Int index = GridObject.instance.GetGridIndex(mouseOverPosition);
-				bool canPlace=commandData.CanPlace(index);
-
+				bool canPlace = commandData.CanPlace(index);
 				commandData.DoPreview(GridObject.instance.GetGridPosition(index),canPlace);
-				//commandData.DoPreview(mouseOverPosition,canPlace);
-
 				if(Input.GetMouseButtonDown(0)&&canPlace) {
 					var targetElement = GridObject.instance.GetElement(mouseOverPosition);
 					targetElement.AddCommand(commandData);
